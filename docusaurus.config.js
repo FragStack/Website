9bf761336b44d38caaf61d8c6e38902c3a531633
user <email@example.com>
2022-06-04 19:27:08 -0300

    updated config for gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'FragStack',
   tagline: 'Welcome to FragStack Project',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/Website',
+  baseUrl: '/Website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
